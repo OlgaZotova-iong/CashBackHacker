d1da2cbc90a9a7ddaec20f33019d2f466ace92f2
Olga Zotova <olgazotova1976@yandex.ru>
2025-10-31 16:34:59 +0300

    Create test class

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnZeroIfAmountIsMultipleOfThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
+
+    @Test
+    public void shouldCalculateRemainingAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
+
+    @Test
+    public void shouldReturnBoundaryIfAmountIsZero(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
+}
