551ae44926f7c339fdb6bf09672f2d9f94baadef
Olga Zotova <olgazotova1976@yandex.ru>
2025-11-03 17:17:12 +0300

    The tests were successful

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
 package ru.netology.service;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnCorrectRemainderForSmallAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
+
+    @Test
+    public void shouldReturnCorrectRemainderForAmountGreaterThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
+
+    @Test
+    public void shouldReturnCorrectRemainderForLargeAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000001;
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected); // actual, expected
+    }
 }
